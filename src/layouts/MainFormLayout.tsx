b980f24a8bde11e6fb8cb1bf69ac1e8a9bd5b07f
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2022-08-31 15:12:37 +0200

    update images

--- src/layouts/MainFormLayout.tsx
+++ src/layouts/MainFormLayout.tsx
@@ -1,32 +1,25 @@
-import FullHeightWrap from '../components/elements/FullHeightWrap';
-import Carousel from '../components/elements/Carousel';
+import FullHeightWrap from "../components/elements/FullHeightWrap";
+import Carousel from "../components/elements/Carousel";
 
 type Props = {
   background?: string;
   children: React.ReactNode;
 };
 
-const MainFormLayout = ({ background, children }: Props): JSX.Element =>
-  (
-    <FullHeightWrap
-      background={background ?? undefined}
-      darkenValue={0.7}
-    >
-      <div className="auth-box">
-        <Carousel
-          slides={[
-            '/img/slide-example-1.jpg',
-            '/img/slide-example-2.jpg',
-            '/img/slide-example-3.jpg',
-            '/img/slide-example-1.jpg',
-            '/img/slide-example-2.jpg',
-          ]}
-        />
-        <div className="auth-box__content">
-          {children}
-        </div>
-      </div>
-    </FullHeightWrap>
-  );
+const MainFormLayout = ({ background, children }: Props): JSX.Element => (
+  <FullHeightWrap background={background ?? undefined} darkenValue={0.7}>
+    <div className="auth-box">
+      <Carousel
+        slides={[
+          "/img/slide1.png",
+          "/img/slide2.png",
+          "/img/slide3.png",
+          "/img/slide4.png",
+        ]}
+      />
+      <div className="auth-box__content">{children}</div>
+    </div>
+  </FullHeightWrap>
+);
 
 export default MainFormLayout;
